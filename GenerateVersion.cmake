b0c9f55cab62583019ff9fdc25306b4a03fe10f2
unknown321 <unknown321@users.noreply.github.com>
2024-12-11 18:00:55 +0300

    improve version

--- GenerateVersion.cmake
+++ GenerateVersion.cmake
@@ -5,30 +5,27 @@ if (GIT_EXECUTABLE)
             OUTPUT_VARIABLE DIRTY_STRINGS
     )
 
-    execute_process(
-            COMMAND ${GIT_EXECUTABLE} log -1 "--format=%h, %ad" "--date=format:%Y-%m-%d %H:%M"
-            OUTPUT_VARIABLE WAMPY_VERSION
-            RESULT_VARIABLE ERROR_CODE
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-
-    execute_process(
-            COMMAND ${GIT_EXECUTABLE} log -1 "--format=%h"
-            OUTPUT_VARIABLE WAMPY_COMMIT
-            RESULT_VARIABLE ERROR_CODE_COMMIT
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
+    if (NOT DIRTY_STRINGS STREQUAL "")
+        execute_process(
+                COMMAND ${GIT_EXECUTABLE} log -1 "--format=%h-dirty, %ad" "--date=format:%Y-%m-%d %H:%M"
+                OUTPUT_VARIABLE WAMPY_VERSION
+                RESULT_VARIABLE ERROR_CODE
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    else ()
+        execute_process(
+                COMMAND ${GIT_EXECUTABLE} log -1 "--format=%h, %ad" "--date=format:%Y-%m-%d %H:%M"
+                OUTPUT_VARIABLE WAMPY_VERSION
+                RESULT_VARIABLE ERROR_CODE
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    endif ()
 endif ()
+
 
 if (WAMPY_VERSION STREQUAL "")
     set(WAMPY_VERSION unknown)
     message(WARNING "Failed to determine version from Git tags. Using default version \"${WAMPY_VERSION}\".")
-endif ()
-
-if (NOT DIRTY_STRINGS STREQUAL "")
-    string(TIMESTAMP ts)
-    set(WAMPY_VERSION ${WAMPY_COMMIT}-dirty-${ts})
-    message(STATUS dirty)
 endif ()
 
 configure_file(${SRC} ${DST} @ONLY)
