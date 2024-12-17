c2c24e3209c748354e65e6746430f06ea0fa4618
unknown321 <unknown321@users.noreply.github.com>
2024-12-17 03:15:52 +0300

    usused import

--- src/skinVariant.cpp
+++ src/skinVariant.cpp
@@ -1,7 +1,5 @@
 #include "skinVariant.h"
 
-#include "Magick++.h"
-
 bool SkinExists(const std::string &name, SkinList *l, std::string *filepath) {
     for (const auto &s : *l) {
         if (s.name == name) {
